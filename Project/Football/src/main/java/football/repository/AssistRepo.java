08de3d3a2e1e54b04cd5caf832dd19310641c218
Milos <milos.lucic192@gmail.com>
2019-12-06 10:53:57 +0100

    event

--- Project/Football/src/main/java/football/repository/AssistRepo.java
+++ Project/Football/src/main/java/football/repository/AssistRepo.java
@@ -1,9 +1,16 @@
 package football.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.Assist;
+import model.Country;
+import model.Team;
+import model.Teamplayer;
 
 public interface AssistRepo extends JpaRepository<Assist, Integer> {
 
+	List<Assist> findByCountry(Teamplayer t);
+	
 }
