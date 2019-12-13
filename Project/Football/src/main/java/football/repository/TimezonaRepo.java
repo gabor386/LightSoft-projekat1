474884445ec5d3ce92887316dd803527ca60d3e9
Milos <milos.lucic192@gmail.com>
2019-12-13 14:49:31 +0100

    bug fix

--- Project/Football/src/main/java/football/repository/TimezonaRepo.java
+++ Project/Football/src/main/java/football/repository/TimezonaRepo.java
@@ -1,9 +1,14 @@
 package football.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.TimeZona;
 
 public interface TimezonaRepo extends JpaRepository<TimeZona, Integer>{
 
+	List<TimeZona> findBytimeZone (String  timeZone);
 }
+
--- Project/FootballJPA/src/model/Timezona.java
+++ Project/FootballJPA/src/model/Timezona.java
@@ -1,7 +1,19 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
 
 
 /**
@@ -9,18 +21,61 @@ import javax.persistence.*;
  * 
  */
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, 
+allowGetters = true)
 @NamedQuery(name="TimeZona.findAll", query="SELECT t FROM TimeZona t")
 public class TimeZona implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
 	private int idTimeZona;
 
+	@NotBlank
 	private String timeZone;
+	
+	 @Column(nullable = false, updatable = false)
+	    @Temporal(TemporalType.TIMESTAMP)
+	    @CreatedDate
+	    private Date createdAt;
+
+	    @Column(nullable = false)
+	    @Temporal(TemporalType.TIMESTAMP)
+	    @LastModifiedDate
+	    private Date updatedAt;
+	
+	
 
 	public TimeZona() {
 	}
+	
+	
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
 
 	public int getIdTimeZona() {
 		return this.idTimeZona;
